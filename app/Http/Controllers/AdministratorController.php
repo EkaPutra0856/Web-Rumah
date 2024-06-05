d6fe61bf8d1258148932660ca0881979584f8cb7
AenGlinr <delladhela99@gmail.com>
2024-06-05 21:28:48 +0700

    edit logout ke landingpage, edit cancel

--- app/Http/Controllers/AdministratorController.php
+++ app/Http/Controllers/AdministratorController.php
@@ -10,13 +10,15 @@ use Illuminate\Support\Facades\Validator;
 
 class AdministratorController extends Controller
 {
-    public function indexRegister (){
+    public function indexRegister()
+    {
         if (Auth::guard('administrators')->check()) {
             return redirect()->intended('/dashboard');
         }
         return view("register");
     }
-    public function index (){
+    public function index()
+    {
         if (Auth::guard('administrators')->check()) {
             return redirect()->intended('/dashboard');
         }
@@ -76,41 +78,41 @@ class AdministratorController extends Controller
     }
 
     public function login(Request $request)
-{
-    $request->validate([
-        'email' => 'required',
-        'password' => 'required',
-    ]);
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
 
-    $credentials = $request->only('email', 'password');
-    
+        $credentials = $request->only('email', 'password');
 
-    if (Auth::guard('administrators')->attempt($credentials)) {
-        /** @var \App\Models\Administrator $administrator **/
-        $administrator = Auth::guard('administrators')->user();
-        $token = $administrator->createToken('MyApp')->plainTextToken;
-    
-        // Store the token for later use, if needed
-        session(['token' => $token]);
-    
-        return redirect()->intended('/dashboard')->withSuccess('Logged in successfully');
+
+        if (Auth::guard('administrators')->attempt($credentials)) {
+            /** @var \App\Models\Administrator $administrator **/
+            $administrator = Auth::guard('administrators')->user();
+            $token = $administrator->createToken('MyApp')->plainTextToken;
+
+            // Store the token for later use, if needed
+            session(['token' => $token]);
+
+            return redirect()->intended('/dashboard')->withSuccess('Logged in successfully');
+        }
+
+
+        return redirect()->back()->withErrors('Login details are not valid')->withInput();
     }
 
 
-    return redirect()->back()->withErrors('Login details are not valid')->withInput();
-}
 
-    
-
-public function dashboard()
-{
-    if (Auth::guard('administrators')->check()) {
-        $user = Auth::guard('administrators')->user();
-        return view('dashboard', ['user' => $user]);
-    } else {
-        return redirect("/")->withErrors('You are not allowed to access');
+    public function dashboard()
+    {
+        if (Auth::guard('administrators')->check()) {
+            $user = Auth::guard('administrators')->user();
+            return view('dashboard', ['user' => $user]);
+        } else {
+            return redirect("/")->withErrors('You are not allowed to access');
+        }
     }
-}
 
 
     public function logout(Request $request)
@@ -118,19 +120,19 @@ public function dashboard()
         Auth::guard('administrators')->logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-    
-        return redirect('/login')->withSuccess('Logged out successfully');
 
-
+        return redirect('/')->withSuccess('Logged out successfully');
     }
-    public function loginFormRegAdmin(){
+    public function loginFormRegAdmin()
+    {
         if (Auth::guard('administrators')->check() || Auth::guard('regadmin')->check()) {
             return redirect()->intended('/dashboard-adminwilayah');
         }
         return view('loginRegionalAdmin');
     }
 
-    public function loginRegAdmin (Request $request) {
+    public function loginRegAdmin(Request $request)
+    {
         $request->validate([
             'email' => 'required',
             'password' => 'required'
@@ -142,24 +144,24 @@ public function dashboard()
             /** @var \App\Models\RegionalAdmin $regadmin **/
             $regadmin = Auth::guard('regadmin')->user();
             $token = $regadmin->createToken('MyApp')->plainTextToken;
-        
+
             // Store the token for later use, if needed
             session(['token' => $token]);
-        
+
             return redirect()->intended('/dashboard-adminwilayah')->withSuccess('Logged in successfully');
         }
 
         return redirect()->intended('/login-regadmin')->withSuccess('Logged in Failed');
-
     }
 
-    public function adminwilayah (){
+    public function adminwilayah()
+    {
         if (Auth::guard('regadmin')->check()) {
             return view('dashboardAdmin');
         }
         return view("loginRegionalAdmin");
     }
-    
+
 
 
     // public function indextable(){
@@ -176,12 +178,12 @@ public function dashboard()
     //     $userId = Auth::guard('administrators')->user()->id;
 
     //     $admin = new Administrator();
-       
+
     //         $admin->name = $request->name;
     //         $admin->email = $request->email;
     //         $admin->password = $request->password;
     //         $admin->notelp = $request->notelp;
-        
+
 
     //     $admin -> save();
     //     session()->flash('success', 'Save Data Successfully!');
