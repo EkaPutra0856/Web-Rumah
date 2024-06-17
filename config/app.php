dde508275cd08a015e38036db776905d56f13c4d
EkaPutra0856 <ravivraft700@gmail.com>
2024-06-17 19:11:25 +0700

    import export

--- config/app.php
+++ config/app.php
@@ -122,13 +122,7 @@ return [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
-    'providers' => [
-    Barryvdh\DomPDF\ServiceProvider::class,
-],
-
-'aliases' => [
-    'PDF' => Barryvdh\DomPDF\Facade\Pdf::class,
-],
+    
 
 
 
